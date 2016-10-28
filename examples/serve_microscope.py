3d98f06ae71b25c95ed53e61dfec57451c45d77e
jubujjamer <jubujjamer@yahoo.com.ar>
2016-10-28 18:24:38 -0300

    Created an examples folder with many ways this system works. Left sample.py in the main folder to have something to work with.

---
+++ examples/serve_microscope.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" File serve_microscope.py
+
+Last update: 28/10/2016
+To be used as a remote server for a microscope.
+Run this file on a server connected to the microcope. This will initialize the
+camera and serial devices.
+
+Usage:
+
+"""
+from pyfpm.web import create_server
+import pyfpm.local as local
+from pyfpm.devices import LedAim, Camera
+
+# Find serial controller of the light supply
+ser_dev = '/dev/ttyACM'
+try:
+    ledaim = LedAim(port=ser_dev+str(0), theta=0, phi=0, power=0, color="red")
+except:
+    print(ser_dev+str(0) + " not available, testing with another.")
+    ledaim = LedAim(port=ser_dev+str(1), theta=0, phi=0, power=0, color="red")
+# Run the camera with open cv
+cam = Camera(video_id=0)
+client = local.LedClient(cam, ledaim)
+app = create_server(client)
+app.run(host='0.0.0.0')
