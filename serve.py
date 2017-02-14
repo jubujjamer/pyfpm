07b3385a3931f627d5b0dc0d24ac08521aa135d0
jubujjamer <jubujjamer@yahoo.com.ar>
2017-02-14 14:04:11 -0300

    Error corrected

--- serve.py
+++ serve.py
@@ -18,13 +18,13 @@ from pyfpm.devices import LedAim, Camera
 
 # Find serial controller of the light supply
 config_dict = yaml.load(open('config.yaml', 'r'))
-ser_dev = config_dict['ser_dev']
+serialport = config_dict['serialport']
 try:
-    ledaim = LedAim(port=ser_dev+str(0), theta=0, phi=0,
+    ledaim = LedAim(port=serialport+str(0), theta=0, phi=0,
                     power=0, color=config_dict['color'])
 except:
-    print(ser_dev+str(0) + " not available, testing with another.")
-    ledaim = LedAim(port=ser_dev+str(1), theta=0, phi=0,
+    print(serialport+str(0) + " not available, testing with another.")
+    ledaim = LedAim(port=serialport+str(1), theta=0, phi=0,
                     power=0, color=config_dict['color'])
 # Run the camera with open cv
 cam = Camera(video_id=config_dict['video_id'], camtype='opencv')
