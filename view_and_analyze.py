8e350aec7554a9c1b18ae95c9b3a5cfdd1b389f3
Juan Bujjamer <jubujjamer@yahoo.com.ar>
2017-04-14 20:51:33 -0300

    Similarity measurement testing.

--- view_and_analyze.py
+++ view_and_analyze.py
@@ -19,7 +19,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from scipy import misc
+from scipy import misc, signal
 import numpy as np
 import time
 import yaml
@@ -37,19 +37,18 @@ cfg = dt.load_config(CONFIG_FILE)
 simclient = local.SimClient(cfg=cfg)
 pc = PlatformCoordinates(theta=0, phi=0, height=cfg.sample_height, cfg=cfg)
 
-out_file = os.path.join(cfg.output_sample,
-                        '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now()))
-in_file = os.path.join(cfg.output_sample,
-                        '2017-04-05_11_36_01.npy')
+in_file = os.path.join(cfg.output_sample, '2017-04-05_113601.npy')
 iterator = fpm.set_iterator(cfg)
 
 image_dict = np.load(in_file)[()]
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 15))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 15))
 plt.grid(False)
 fig.show()
+cum_corr = 0
+
 for index, theta, phi in iterator:
     # Coordinates math
-    time.sleep(0.5)
+    # time.sleep(0.5)
     pc.platform_tilt = [0, 2]
     pc.source_center = [0, 3] #[xcoord, ycoord] of the calibrated center
     pc.set_coordinates(theta=theta, phi=phi, units='degrees')
@@ -60,13 +59,27 @@ for index, theta, phi in iterator:
     sim_im_array = simclient.acquire(t_corr+theta_sim_offset, p_corr, power)
     im_array = image_dict[(theta, phi)]
     im_array = fpm.crop_image(im_array, cfg.video_size, 170, 245)
-    ax1.cla(), ax2.cla()
+    # im_array = sim_im_array
+    # sim_im_array /= np.mean(sim_im_array)
+    # im_array /= np.mean(im_array)
+
+    corr = signal.correlate2d(im_array[40:110, 40:110], sim_im_array[40:110, 40:110], mode='same', boundary='fill', fillvalue=0)
+    # corr /= (np.sum(im_array+sim_im_array))
+    cum_corr += np.sum(corr)
+    print(cum_corr, np.log10(cum_corr))
+    if theta == 20 or theta == 340:
+        print("Cummulative in circle", cum_corr, np.log10(cum_corr))
+    # Showing the images
+    ax1.cla(), ax2.cla(), ax3.cla()
     ax1.imshow(im_array, cmap=cm.hot)
-    ax1.annotate('Mean value: %.4f \nPHI: %.1f THETA: %.1f'% (np.mean(im_array), phi, theta),
-                  xy=(0,0), xytext=(80,10), fontsize=12, color='white')
+    ax1.annotate('Mean value: %.4f \nPHI: %.1f THETA: %.1f'% (np.max(im_array), phi, theta),
+                  xy=(0,0), xytext=(80,20), fontsize=12, color='white')
     ax2.imshow(sim_im_array, cmap=cm.hot)
-    ax2.annotate('Mean value: %.4f \nPHI: %.1f THETA: %.1f'% (np.mean(sim_im_array), phi, theta),
-                 xy=(0,0), xytext=(80,10), fontsize=12, color='white')
+    ax2.annotate('Mean value: %.4f \nPHI: %.1f THETA: %.1f'% (np.max(sim_im_array), phi, theta),
+                 xy=(0,0), xytext=(80,20), fontsize=12, color='white')
+    ax3.imshow(corr, cmap=cm.hot, vmin = 0)
+    ax3.annotate('Mean value: %.2f \nMax value: %.2f'% (np.mean(corr), np.max(corr)),
+                 xy=(0,0), xytext=(40,20), fontsize=12, color='white')
 
     fig.canvas.draw()
 
